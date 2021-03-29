f202d12185ac37a79135826f5605842925e655e0
Andrea <andrear632@gmail.com>
2021-03-29 12:23:28 +0200

    send mail node

---
+++ send_mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'andrear632@gmail.com',
+    pass: 'nphubonhakpfcbhv'
+  }
+});
+
+var mailOptions = {
+  from: 'andrear632@gmail.com',
+  to: 'giorgia.ristich@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
