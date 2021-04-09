67564c9420a4de4e173a781eb8365399002a176d
chiarizz <chia.rizzato@gmail.com>
2021-04-09 13:30:28 +0200

    modifiche

---
+++ function.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var input=document.getElementById("pass");
+    if(input.type==="password") {
+        input.type="text";
+    }else {
+        input.type="password";
+    }
+}
